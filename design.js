872c6a038f714faa9a02d2144481b2b632b3e1cb
Jai Primm <jailaprimm99@gmail.com>
2022-07-09 12:20:28 -0500

    updated js

--- design.js
+++ design.js
@@ -1,15 +1,42 @@
-$(document).ready
 
-// Select color input
-// Select size input
+const $colorPicker = $('#colorPicker');
+const $tableElement = $('#pixelCanvas');
+const $inputHeight = $('#inputHeight');
+const $inputWidth = $('#inputWidth');
 
-// When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+$('#sizePicker').submit( event => {
+    event.preventDefault();
 
-    // Your code goes here!
+    let width = $inputWidth.val();
+    let height = $inputHeight.val();
+
+    //press sumbit to clear coloring off chart. //
+    $tableElement.html(''); //clear
+
+    makeGrid(height, width);
+    addCellClickListener();
+});
+
+//height and width choosen by guest to create numbers of pixel of choice//
+function makeGrid(height, width) {
+    for(let i = 0; i < height; i++) {
+        $tableElement.append('<tr></tr>');
+    };
+
+    for(let i = 0; i < width; i++) {
+        $('tr').append('<td></td>');
+    };
+};
+
+//pixel boxs colors. guests can change colors//
+function addCellClickListener() {
+    $('td').click( event => {
+        let color = $colorPicker.val();
+        $(event.currentTarget).css("background-color", color)
+    });
+};
     
-    }
 
-    
+   
     
